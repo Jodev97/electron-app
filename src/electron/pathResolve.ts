5b64756667e47358a089553a170a32861ccf9bfe
jairo andres quintero <jairoquinterodev@gmail.com>
2024-12-25 13:38:30 -0500

    Add getUIPath function and validateEventFrame function

--- src/electron/pathResolve.ts
+++ src/electron/pathResolve.ts
@@ -2,10 +2,14 @@ import { app } from "electron";
 import path from "path";
 import { isDev } from "./util.js";
 
-export function getPreloadPath(){
+export function getPreloadPath() {
     return path.join(
         app.getAppPath(),
-        isDev() ? '.': ':',
+        isDev() ? '.' : ':',
         '/dist-electron/preload.cjs'
     )
+}
+
+export function getUIPath() {
+    return path.join(app.getAppPath(), '/dist-react/index.html')
 }
--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,11 +1,16 @@
-import { WebContents, ipcMain } from "electron";
+import { WebContents, WebFrameMain, ipcMain } from "electron";
+import { getUIPath } from "./pathResolve.js";
+import { pathToFileURL } from 'url';
 
 export const isDev = (): boolean => {
     return process.env.NODE_ENV === 'development';
 }
 
 export function ipcMainHandle<Key extends keyof EventPayloadMapping>(key: Key, handler: () => EventPayloadMapping[Key]) {
-    ipcMain.handle(key, async () => handler());
+    ipcMain.handle(key, async (event) => {
+        validateEventFrame(event.senderFrame);
+        return handler()
+    });
 }
 
 export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
@@ -13,4 +18,18 @@ export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
     webContents: WebContents,
     payload: EventPayloadMapping[Key]) {
     webContents.send(key, payload);
+}
+
+export function validateEventFrame(frame: WebFrameMain | null) {
+
+    if (!frame) {
+        throw new Error('Malicius event');
+    }
+
+    if (isDev() && new URL(frame.url).host === 'localhost:5123') {
+        return;
+    }
+    if (frame.url !== pathToFileURL(getUIPath()).toString()) {
+        throw new Error('Malicius event');
+    }
 }
